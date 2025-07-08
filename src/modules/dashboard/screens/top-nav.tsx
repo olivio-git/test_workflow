0679c83e4a7dd689f8728457f24163157d2be364
Limber Tolaba <limber.rodrigo.tj@gmail.com>
2025-07-08 13:40:43 -0400

    Buscador Ctrl+K

--- src/modules/dashboard/screens/top-nav.tsx
+++ src/modules/dashboard/screens/top-nav.tsx
@@ -1,32 +1,43 @@
-"use client"
+"use client";
 
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@/components/atoms/dropdown-menu"
-import { Bell, ChevronRight } from "lucide-react"
-import Profile01 from "./profile-01"
-import { Image } from "@radix-ui/react-avatar"
-import { Link } from "react-router"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/atoms/dropdown-menu";
+import { Bell, ChevronRight } from "lucide-react";
+import Profile01 from "./profile-01";
+//import { Image } from "@radix-ui/react-avatar"
+import { Link } from "react-router";
+import CommandPalette from "./CommandPalette/CommandPalette";
+import SearchButton from "./CommandPalette/SearchButton";
+import { useState } from "react";
 
 interface BreadcrumbItem {
-  label: string
-  href?: string
+  label: string;
+  href?: string;
 }
 
 export default function TopNav() {
   const breadcrumbs: BreadcrumbItem[] = [
     { label: "kokonutUI", href: "#" },
     { label: "dashboard", href: "#" },
-  ]
+  ];
+    const [open, setOpen] = useState(false);
+
 
   return (
-    <nav className="px-3 sm:px-6 flex items-center justify-between bg-white border-b border-gray-200 h-full">
+    <nav className="flex items-center justify-between h-full px-3 bg-white border-b border-gray-200 sm:px-6">
       <div className="font-medium text-sm hidden sm:flex items-center space-x-1 truncate max-w-[300px]">
         {breadcrumbs.map((item, index) => (
           <div key={item.label} className="flex items-center">
-            {index > 0 && <ChevronRight className="h-4 w-4 text-gray-500 mx-1" />}
+            {index > 0 && (
+              <ChevronRight className="w-4 h-4 mx-1 text-gray-500" />
+            )}
             {item.href ? (
               <Link
                 to={item.href}
-                className="text-gray-700 hover:text-gray-900 transition-colors"
+                className="text-gray-700 transition-colors hover:text-gray-900"
               >
                 {item.label}
               </Link>
@@ -36,19 +47,22 @@ export default function TopNav() {
           </div>
         ))}
       </div>
-        <div>BAR</div>
-      <div className="flex items-center gap-2 sm:gap-4 ml-auto sm:ml-0">
+      <div className="flex items-center gap-4">
+        <SearchButton onClick={() => setOpen(true)} />
+        <CommandPalette open={open} setOpen={setOpen} />
+      </div>
+
+
+      <div className="flex items-center gap-2 ml-auto sm:gap-4 sm:ml-0">
         <button
           type="button"
           className="p-1.5 sm:p-2 hover:bg-gray-100 rounded-full transition-colors"
         >
-          <Bell className="h-4 w-4 sm:h-5 sm:w-5 text-gray-600" />
+          <Bell className="w-4 h-4 text-gray-600 sm:h-5 sm:w-5" />
         </button>
 
-
         <DropdownMenu>
-          <DropdownMenuTrigger className="focus:outline-none"> 
-          </DropdownMenuTrigger>
+          <DropdownMenuTrigger className="focus:outline-none"></DropdownMenuTrigger>
           <DropdownMenuContent
             align="end"
             sideOffset={8}
@@ -59,5 +73,5 @@ export default function TopNav() {
         </DropdownMenu>
       </div>
     </nav>
-  )
+  );
 }
