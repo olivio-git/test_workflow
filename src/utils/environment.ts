5344e2b544b080512b1ac252c0199b0850852f0e
olivio-dev <tj.olivio.subelza.c@upds.net.bo>
2025-10-02 23:09:41 -0400

    release: versión 1.0.6

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   apiUrl: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
-  branch_selected_key: import.meta.env.BRANCH_STORAGE_KEY || 'key_branch',
+  branch_selected_key: import.meta.env.VITE_BRANCH_STORAGE_KEY || 'key_branch',
   app_env: import.meta.env.VITE_APP_ENV || 'production',
 };
